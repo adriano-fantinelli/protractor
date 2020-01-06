86a2f228ab7bb2bad36b491b9d5eb52b16113b22
Adriano Fantinelli <adrianofa2013@gmail.com>
2020-01-06 15:27:26 -0300

    correção

--- tutorial-protractor/pageObjects/loginPage.po.js
+++ tutorial-protractor/pageObjects/loginPage.po.js
@@ -2,18 +2,25 @@ const {element, browser } = require("protractor");
 
 class LoginPage{
     
-    get etEmail() { return element(by.css('#email')) }
+  get etEmail() { return element(by.css('#email')) }
 
-    get etSenha() { return element(by.css('#passwd')) }
+  get etSenha() { return element(by.css('#passwd')) }
 
-    get btnSignIn() { return element(by.css('#SubmitLogin')) }    
+  get btnSignIn() { return element(by.css('#SubmitLogin')) }    
 
-    async login(email, senha) {
-      this.etEmail.sendKeys(email);
-      this.etSenha.sendKeys(senha);
-      this.btnSignIn.click();
+  get alertErro() { return element(by.css('.alert-danger > ol > li')) }
+
+  async login(email, senha) {
+    browser.wait(ExpectedConditions.elementToBeClickable(this.etEmail), 3000)
+    this.etEmail.sendKeys(email);
+    this.etSenha.sendKeys(senha);
+    this.btnSignIn.click();
 }
 
+  async getMensagemErro(){
+    browser.wait(ExpectedConditions.elementToBeClickable(this.alertErro), 3000)
+    console.log(await this.alertErro.getText());
+    return await this.alertErro.getText();
+  }
 }
-
 module.exports = new LoginPage();
