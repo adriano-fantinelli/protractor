484e388968d75970346332c6e8d2c3f1f7a4c722
Adriano Fantinelli <adrianofa2013@gmail.com>
2020-01-07 10:46:14 -0200

    indentação

--- pageObjects/homePage.po.js
+++ pageObjects/homePage.po.js
@@ -2,13 +2,13 @@ const {element, browser } = require("protractor");
 
 class HomePage{
     
-    get btnLogin() { return element(by.css('.login')) }
+  get btnLogin() { return element(by.css('.login')) }
 
-    async visit() {
-      browser.ignoreSynchronization = true;
-      browser.driver.manage().window().setSize(1920, 980);
-      browser.manage().window().maximize();            
-      browser.get('http://automationpractice.com/index.php')
+  async visit() {
+    browser.ignoreSynchronization = true;
+    browser.driver.manage().window().setSize(1920, 980);
+    browser.manage().window().maximize();            
+    browser.get('http://automationpractice.com/index.php')
   }
 
 }
